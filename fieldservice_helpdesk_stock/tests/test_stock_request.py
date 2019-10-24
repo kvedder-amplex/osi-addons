3ac97c799ebceffb61e61966995a23181c13f32d
scampbell <scampbell@opensourceintegrators.com>
2019-10-24 09:56:26 -0700

    [IMP] Cosmetics

--- fieldservice_helpdesk_stock/tests/test_stock_request.py
+++ fieldservice_helpdesk_stock/tests/test_stock_request.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright (C) 2019 Open Source Integrators
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from odoo.tests import common
